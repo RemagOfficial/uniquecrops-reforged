d84a5d2f1b1d2e47b10949c438547d68bca76ceb
Jeff Alexander <kiteserpent@gmail.com>
2025-08-29 17:10:38 -0700

    Don't transform to OR FROM bosses. Add Warden to boss list.

--- src/main/java/com/remag/ucse/items/curios/EmblemTransformation.java
+++ src/main/java/com/remag/ucse/items/curios/EmblemTransformation.java
@@ -6,6 +6,8 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.entity.monster.warden.Warden;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
@@ -22,9 +24,12 @@ public class EmblemTransformation extends ItemCurioUC {
     }
 
     private void onHitEntity(LivingHurtEvent event) {
-        if (event.getAmount() <= 0 || event.getEntity() instanceof Player) return;
+        if (event.getAmount() <= 0) return;
         if (!(event.getSource().getDirectEntity() instanceof Player)) return;
         if (!hasCurio((LivingEntity) event.getSource().getDirectEntity())) return;
+        Entity entityHurt = event.getEntity();
+        if (entityHurt instanceof Player || entityHurt instanceof WitherBoss ||
+                entityHurt instanceof EnderDragon || entityHurt instanceof Warden) return;
 
         Random rand = new Random();
         if (rand.nextInt(100) == 0) {
@@ -35,6 +40,8 @@ public class EmblemTransformation extends ItemCurioUC {
                     .filter(type -> type.create(elb.level()) instanceof LivingEntity) // only spawn living entities
                     .filter(type -> !(type.create(elb.level()) instanceof WitherBoss)) // exclude Wither
                     .filter(type -> !(type.create(elb.level()) instanceof EnderDragon)) // exclude Dragon
+                    .filter(type -> !(type.create(elb.level()) instanceof Warden)) // exclude Warden
+                    .filter(type -> !(type.create(elb.level()) instanceof ArmorStand)) // armor stands are alive!
                     .toList();
 
             if (entityTypes.isEmpty()) return;
